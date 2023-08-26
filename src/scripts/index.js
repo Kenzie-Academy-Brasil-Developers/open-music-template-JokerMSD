f7d4b8cbfb7b171fd7971f04a763d009c81df7a5
JokerMSD <igorpn2004@gmail.com>
2023-08-26 13:07:41 -0300

    updated

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,164 @@
 /* Desenvolva sua lógica aqui ... */
+
+const createCard = (product, mode) => {
+  const card = document.createElement("li");
+  card.classList.add("card");
+  card.classList.add(mode);
+
+  const image = document.createElement("img");
+  image.classList.add("img-card");
+  image.src = product.img;
+  image.alt = product.title;
+  card.appendChild(image);
+
+  const infoContainer = document.createElement("div");
+  infoContainer.classList.add("divband");
+  infoContainer.classList.add(mode);
+
+  const bandAndYear = document.createElement("p");
+  bandAndYear.classList.add("band");
+  bandAndYear.classList.add(mode);
+
+  bandAndYear.textContent = `${product.band} - ${product.year}`;
+
+  const title = document.createElement("h2");
+  title.classList.add("bandtitle");
+  title.classList.add(mode);
+  title.textContent = product.title;
+
+  const priceAndButton = document.createElement("span");
+  priceAndButton.classList.add("priceAndButton");
+  priceAndButton.classList.add(mode);
+
+  const price = document.createElement("p");
+  price.classList.add("price");
+  price.classList.add(mode);
+  price.textContent = `R$ ${product.price.toFixed(2)}`;
+
+  const buyButton = document.createElement("button");
+  buyButton.classList.add("buybutton");
+  buyButton.classList.add(mode);
+  buyButton.textContent = "Comprar";
+
+  priceAndButton.appendChild(price);
+  priceAndButton.appendChild(buyButton);
+
+  infoContainer.appendChild(priceAndButton);
+  infoContainer.appendChild(title);
+  infoContainer.appendChild(bandAndYear);
+
+  card.appendChild(infoContainer);
+
+  return card;
+};
+
+const renderCards = (products, mode) => {
+  const cardContainer = document.querySelector(".album-section__album-cards");
+
+  products.forEach((product) => {
+    const card = createCard(product, mode);
+    cardContainer.appendChild(card);
+  });
+};
+
+renderCards(products);
+
+const renderFilterButtons = (categories) => {
+  const buttonContainer = document.querySelector(".filter-section__genre-list");
+
+  categories.forEach((category) => {
+    const listItem = document.createElement("li");
+
+    const button = document.createElement("button");
+    button.classList.add("genre_button");
+    button.classList.add("active");
+    button.classList.add("light-mode");
+
+    button.textContent = category;
+
+    listItem.appendChild(button);
+    buttonContainer.appendChild(listItem);
+  });
+};
+
+renderFilterButtons(categories);
+
+import toggleTheme from "./theme.js";
+
+const toggleButton = document.querySelector(".header__dark-mode-button");
+
+toggleButton.addEventListener("click", toggleTheme);
+
+const priceRangeInput = document.querySelector(".filter-section__price-range");
+const priceText = document.querySelector(".filter-section__price-text");
+const ul = document.querySelector(".album-section__album-cards");
+
+const formatPrice = (value) => {
+  const intValue = Math.floor(value);
+  const decimalPart = (value - intValue).toFixed(2).slice(2);
+  const formattedValue = `${
+    intValue < 10 ? "0" : ""
+  }${intValue}.${decimalPart}`;
+  return formattedValue;
+};
+
+const updatePriceText = () => {
+  const priceRangeInputValue = parseFloat(priceRangeInput.value);
+  const priceRangeInputFixed = formatPrice(priceRangeInputValue);
+  const priceTextFixed = priceText;
+  priceTextFixed.textContent = `Até R$: ${priceRangeInputFixed}`;
+};
+const body = document.querySelector(".body");
+
+const clearCardContainer = () => {
+  const cardContainer = document.querySelector(".album-section__album-cards");
+  cardContainer.innerHTML = "";
+};
+
+function applyFilters(price, genre) {
+  clearCardContainer();
+
+  const filteredByPrice =
+    price > 0 ? products.filter((product) => product.price <= price) : products;
+
+  const filteredProducts =
+    genre === "Todos"
+      ? filteredByPrice
+      : filteredByPrice.filter(
+          (product) => product.category === categories.indexOf(genre)
+        );
+
+  renderCards(
+    filteredProducts,
+    body.classList.contains("dark-mode") ? "dark-mode" : "light-mode"
+  );
+}
+
+priceRangeInput.addEventListener("input", (e) => {
+  e.preventDefault();
+  updatePriceText();
+
+  const priceRangeInputValue = parseFloat(priceRangeInput.value);
+  const selectedGenre = document.querySelector(
+    ".genre_button.active"
+  )?.textContent;
+
+  applyFilters(priceRangeInputValue, selectedGenre);
+});
+
+const genreButtons = document.querySelectorAll(".genre_button");
+
+genreButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    genreButtons.forEach((btn) => btn.classList.remove("active"));
+    button.classList.add("active");
+
+    const selectedGenre = button.textContent;
+    const priceRangeInputValue = parseFloat(priceRangeInput.value);
+    applyFilters(priceRangeInputValue, selectedGenre);
+  });
+});
+
+
+updatePriceText();
+toggleTheme();
--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,1 +1,77 @@
-/* Desenvolva sua lógica aqui ... */
+// theme.js
+const body = document.body;
+const darkModeButton = document.querySelector(".header__dark-mode-button");
+const logoImage = document.querySelector(".header__img-logo");
+const header = document.querySelector(".header__logo");
+const albumTitle = document.querySelector(".album-section__title");
+
+const adjustElements = () => {
+  const elementsToAdjust = [
+    body,
+    albumTitle,
+    header,
+    darkModeButton,
+    ...document.querySelectorAll(".band"),
+    ...document.querySelectorAll(".bandtitle"),
+    ...document.querySelectorAll(".price"),
+    ...document.querySelectorAll(".buybutton"),
+    ...document.querySelectorAll(".divband"),
+    ...document.querySelectorAll(".genre_button"),
+  ];
+
+  elementsToAdjust.forEach((element) => {
+    element.classList.toggle(
+      "light-mode",
+      body.classList.contains("light-mode")
+    );
+    element.classList.toggle("dark-mode", body.classList.contains("dark-mode"));
+  });
+
+  logoImage.src = body.classList.contains("dark-mode")
+    ? "./src/assets/img/day.png"
+    : "./src/assets/img/night.png";
+
+  localStorage.setItem(
+    "theme",
+    body.classList.contains("dark-mode") ? "dark" : "light"
+  );
+};
+
+const enableDarkMode = () => {
+  body.classList.add("dark-mode");
+  body.classList.remove("light-mode");
+  adjustElements();
+  logoImage.src = "./src/assets/img/day.png";
+  localStorage.setItem("theme", "dark");
+};
+
+const enableLightMode = () => {
+  body.classList.add("light-mode");
+  body.classList.remove("dark-mode");
+  adjustElements();
+  logoImage.src = "./src/assets/img/night.png";
+  localStorage.setItem("theme", "light");
+};
+
+const toggleTheme = () => {
+  if (body.classList.contains("dark-mode")) {
+    enableLightMode();
+  } else {
+    enableDarkMode();
+  }
+};
+
+// Carregar o tema salvo do LocalStorage ao carregar a página
+document.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      enableDarkMode();
+    } else {
+      enableLightMode();
+    }
+    adjustElements(); 
+  });
+
+  darkModeButton.addEventListener("click", toggleTheme);
+
+export default toggleTheme;
